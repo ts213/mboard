2179bd25b081e258a964ed1f5eba8eab715d362e
ts213 <107356620+ts213@users.noreply.github.com>
2023-02-13 19:52:31 +0300

    form finished ...?

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -81,53 +81,41 @@ export function PostForm() {
       <div className='min-w-max'>
         {attachedFiles()?.map((file, idx) => {
             let fileUrl = URL.createObjectURL(file);
-            return <picture key={idx}
-              // className='mr-8 after:content-["X"] after:font-extrabold after:text-red-500 after:-m-4'>
-              className='mr-8 relative overflow-visible w-fit inline-block'>
-              <img src={fileUrl} title={file.name} alt='img-preview'
-                onClick={() => removeFileFromFileList(idx)}
-                style={{ maxWidth: '100px', maxHeight: '100px', display: 'inline' }}
-              />
-              <span className='absolute right-2 pointer-events-none text-red-400 font-bold'>X</span>
-            </picture>;
+            return (
+              <picture key={idx}
+                onClick={() => removeFileFromFileList(idx, fileUrl)}
+                className='mr-2 relative w-fit inline-block'
+              >
+                <source srcSet={fileUrl} type={file.type} title={file.name}
+                  style={{ maxWidth: '100px', maxHeight: '100px', display: 'inline' }}
+                />
+                <img src='' title={file.name}  // without 'alt' so as to display an empty box
+                  style={{ width: '100px', height: '100px', display: 'inline' }}
+                />
+                <span className='absolute right-2 pointer-events-none text-red-400 font-bold'>X</span>
+              </picture>
+            )
           }
         )}
       </div>
 
-      {/*{attachedFiles()?.map((fileName, idx) =>*/}
-      {/*  <output key={idx}*/}
-      {/*    onClick={() => removeFileFromFileList(idx)}*/}
-      {/*    className='block pointer-events-none text-center text-gray-400 mt-1 after:content-["[⨯]"] after:ml-2 after:pointer-events-auto'>*/}
-      {/*    {fileName}*/}
-      {/*  </output>*/}
-      {/*)}*/}
-
-      <input hidden name='board' readOnly value={board} />
-      <input hidden name='threadId' readOnly value={threadId} />
+      <input type='hidden' name='board' readOnly value={board} />
+      <input type='hidden' name='threadId' readOnly value={threadId} />
     </fetcher.Form>
   );
 
   function attachedFiles() {
-    // if (fileList.length > 0) {
-    //   const file = fileList[0];
-    //   thumbsRef.current.src = URL.createObjectURL(file);
-    // }
-
-    // return fileList.length < 1 ? null
-    //   : fileList.map(file => file.name);
     return fileList.length < 1 ? null
       : fileList.map(file => file);
   }
 
-  function removeFileFromFileList(idxToRemove) {
+  function removeFileFromFileList(idxToRemove, fileUrl) {
     const dt = new DataTransfer(); // workaround bc can't change input.files directly
     const nextFileList = fileList.filter((_, idx) => idx !== idxToRemove);  // need sync. value instead of async
     nextFileList.forEach(file => dt.items.add(file));
     inputRef.current.files = dt.files;
 
+    URL.revokeObjectURL(fileUrl);
     setFileList(nextFileList);
   }
-
 }
-
-
